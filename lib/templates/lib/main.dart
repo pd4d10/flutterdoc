296c0355bf2885c8a281b1cf2af871e78f8e89e0
Rongjian Zhang <pd4d10@gmail.com>
2019-09-09 19:23:10 +0800

    chore: add flutter web templates

---
+++ lib/templates/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_web/material.dart';
+import 'widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/templates/lib/widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_web/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
