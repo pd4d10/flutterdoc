3f649bf6f6329d9e755ddcaf98435ff4b989f47f
Rongjian Zhang <pd4d10@gmail.com>
2019-09-22 15:55:46 +0800

    refactor: using args for cli

--- bin/flutterdoc.dart
+++ bin/flutterdoc.dart
@@ -1,5 +1,32 @@
+import 'package:args/command_runner.dart';
 import 'package:flutterdoc/flutterdoc.dart' as flutterdoc;
 
+class ServeCommand extends Command {
+  @override
+  String get name => 'serve';
+  @override
+  String get description => 'Serve';
+
+  run() {
+    flutterdoc.serve();
+  }
+}
+
+class BuildCommand extends Command {
+  @override
+  String get name => 'build';
+  @override
+  String get description => 'Build';
+
+  run() {
+    flutterdoc.build();
+  }
+}
+
 main(List<String> arguments) {
-  flutterdoc.build();
+  var runner = CommandRunner('flutterdoc', 'Generate gallery for your widgets')
+    ..addCommand(ServeCommand())
+    ..addCommand(BuildCommand());
+
+  runner.run(arguments);
 }
