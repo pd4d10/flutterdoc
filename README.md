493ff6fe2ad2616c50ab062889c5ad2c738628e8
Rongjian Zhang <pd4d10@gmail.com>
2019-09-23 15:21:42 +0800

    build: update script, add readme link

---
+++ README.md
@@ -0,0 +1,1 @@
+flutterdoc/README.md
--- flutterdoc/tool/template.dart
+++ flutterdoc/tool/template.dart
@@ -5,11 +5,11 @@ main(List<String> args) async {
   var code = 'const template = {';
   ['pubspec.yaml', 'lib/main.dart', 'lib/examples.dart', 'lib/payloads.dart']
       .forEach((name) {
-    var content = File('templates/gallery/$name')
-        .readAsStringSync()
-        .replaceAll(r'$', r'\$');
+    var content =
+        File('gallery/$name').readAsStringSync().replaceAll(r'$', r'\$');
     code += "'$name': '''$content''',";
   });
   code += '};';
-  File('lib/src/template.dart').writeAsStringSync(DartFormatter().format(code));
+  File('flutterdoc/lib/src/template.dart')
+      .writeAsStringSync(DartFormatter().format(code));
 }
